949e520d087c0b95a0d283a3cc80fcdfef2974d6
chayan-DUET <chayan_cse10@yahoo.com>
2022-04-09 09:24:59 +0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const mysql = require("mysql");
+const userRoute = require('./route/route.user');
+
+// parse application/json
+app.use(bodyParser.json());
+
+const conn = require("./db/database.connection")
+/* //Create Database Connection
+const conn = mysql.createConnection({
+	host: "localhost",
+	user: "root",
+	password: "Chayan@2020",
+	database: "crud",
+});
+
+// connect to database
+conn.connect((err) => {
+	if (err) throw err;
+	console.log("MySQL is connected");
+}); */
+
+// creat a new Record
+app.use(userRoute);
+
+// show all records
+app.get("/api/view", (req, res) => {
+	let sql = "SELECT * FROM users";
+	let query = conn.query(sql, (err, result) => {
+		if (err) throw err;
+		res.send(JSON.stringify({ status: 200, error: null, response: result }));
+	});
+});
+
+// show a single record
+app.get("/api/view/:id", (req, res) => {
+	let sql = "SELECT * FROM users WHERE id=" + req.params.id;
+	let query = conn.query(sql, (err, result) => {
+		if (err) throw err;
+		res.send(JSON.stringify({ status: 200, error: null, response: result }));
+	});
+});
+
+// delete the record
+app.delete("/api/delete/:id", (req, res) => {
+	let sql = "DELETE FROM users WHERE id=" + req.params.id + "";
+	let query = conn.query(sql, (err, result) => {
+		if (err) throw err;
+		res.send(JSON.stringify({ status: 200, error: null, response: "Record deleted successfully" }));
+	});
+});
+
+// update the Record
+app.put("/api/update/", (req, res) => {
+	let sql = "UPDATE users SET name='" + req.body.name + "', location='" + req.body.location + "' WHERE id=" + req.body.id;
+	let query = conn.query(sql, (err, result) => {
+		if (err) throw err;
+		res.send(JSON.stringify({ status: 200, error: null, response: "Record updated SuccessFully" }));
+	});
+});
+
+app.listen(8000, () => {
+	console.log("server started on port 8000...");
+});
